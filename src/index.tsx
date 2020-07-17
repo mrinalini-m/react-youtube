1cbd4e4a554737cd4d40a780ff83009e647c6dbf
mrinalini-m <mrinalini.manandhar@gmail.com>
2020-07-17 14:52:59 -0700

    Add Material CssBaseline, remove unused styles dir

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
+import CssBaseline from '@material-ui/core/CssBaseline'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
-import './styles/index.css'
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
